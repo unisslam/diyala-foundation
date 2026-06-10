2c2313d25812d8242b8e995d905f19fff42e9b94
unisslam <unisslam@gmail.com>
2026-06-10 09:24:19 +0300

    chore: disable specific typescript and react-hooks linting rules in eslint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -18,5 +18,10 @@ export default defineConfig([
     languageOptions: {
       globals: globals.browser,
     },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'react-hooks/exhaustive-deps': 'off'
+    },
   },
 ])
